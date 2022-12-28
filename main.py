a36c031208f1eb05bd9af236210d11256bd24871
james <james@chsc.tw>
2022-12-28 15:53:58 +0800

    爬了....

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://24h.pchome.com.tw/"
+rsp = requests.get(url)
+
+soup = BeautifulSoup(rsp.text, 'html.parser')
+print(soup.prettify())  # prettify()用來把 html 程式變漂亮
+
